e8a2e63718bef6dd7ca325e479eb7234ef77c020
collectionman <julianllorensi@outlook.com>
2019-05-12 19:44:36 -0300

    why character is a int

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
-// This program take the input by keyboard and then
+// This C program take the input by keyboard and then
 // prints the input on the screen while the input character
 // don't be equal to EOF.
 
 #include <stdio.h>
 
 int main() {
+    // char type cannot be used because que c must be big enough to storage EOF
+    // and other characters. That's why we used int.
     int character ;
+
     for ( character = getchar() ; character != EOF ; character = getchar() ) {
         putchar( character ) ;
     }
